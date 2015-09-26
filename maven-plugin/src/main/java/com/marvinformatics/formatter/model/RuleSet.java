801ae8ebf3247d7f75702caa65b9ec248d02867f
Marvin Froeder <velo.br@gmail.com>
2015-09-26 16:40:05 +1200

    Update RuleSet.java

--- maven-plugin/src/main/java/com/marvinformatics/formatter/model/RuleSet.java
+++ maven-plugin/src/main/java/com/marvinformatics/formatter/model/RuleSet.java
@@ -36,16 +36,16 @@ class RuleSet extends RuleSetBase {
 	 */
 	@Override
 	public void addRuleInstances(Digester digester) {
-		digester.addObjectCreate("profiles", Profiles.class);
-		digester.addObjectCreate("profiles/profile", Profile.class);
-		digester.addObjectCreate("profiles/profile/setting", Setting.class);
+	 	digester.addObjectCreate("profiles", Profiles.class);
+	 	digester.addObjectCreate("profiles/profile", Profile.class);
+	 	digester.addObjectCreate("profiles/profile/setting", Setting.class);
 
-		digester.addSetNext("profiles/profile", "addProfile");
-		digester.addSetNext("profiles/profile/setting", "addSetting");
+	 	digester.addSetNext("profiles/profile", "addProfile");
+	 	digester.addSetNext("profiles/profile/setting", "addSetting");
 
-		digester.addSetProperties("profiles/profile", "kind", "kind");
-		digester.addSetProperties("profiles/profile/setting", "id", "id");
-		digester.addSetProperties("profiles/profile/setting", "value", "value");
+	 	digester.addSetProperties("profiles/profile", "kind", "kind");
+	 	digester.addSetProperties("profiles/profile/setting", "id", "id");
+	 	digester.addSetProperties("profiles/profile/setting", "value", "value");
 	}
 
 }
